c42173e3a031ae06872bd36241f6d0915d0b6754
Ankita Singh <a.singh@planetsmartcity.com>
2023-09-06 14:14:40 +0530

    Removed language from labels

--- resources/views/backend/books_category/edit.blade.php
+++ resources/views/backend/books_category/edit.blade.php
@@ -44,7 +44,7 @@
                                                         <?php foreach ($translated_block as $translated_block_fields_key => $translated_block_fields_value) { ?>
                                                             <?php if($translated_block_fields_value == 'input') { ?>
                                                                 <div class="col-md-6 mb-3">
-                                                                    <label>{{ $translated_block_fields_key }} ({{  config('translatable.locales_name')[$translated_data_tabs] }})</label>
+                                                                    <label>{{ $translated_block_fields_key }}</label>
                                                                     <input class="translation_block form-control required" type="text" id="{{$translated_block_fields_key}}_{{$translated_data_tabs}}" name="{{$translated_block_fields_key}}_{{$translated_data_tabs}}" value="{{$category[$translated_block_fields_key.'_'.$translated_data_tabs] ?? ''}}">
                                                                 </div>
                                                             <?php } ?>
