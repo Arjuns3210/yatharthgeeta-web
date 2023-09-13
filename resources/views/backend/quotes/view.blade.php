16e1281a5ec0dfe015c4e76ad643dc0346f2175d
Aaditi kadam <aaditi.k@mypcot.com>
2023-09-13 13:25:18 +0530

    Quote view blade changes

--- resources/views/backend/quotes/view.blade.php
+++ resources/views/backend/quotes/view.blade.php
@@ -1,50 +1,28 @@
 <section class="users-list-wrapper">
-    <div class="users-list-table">
+	<div class="users-list-table">
         <div class="row">
             <div class="col-12">
                 <div class="card">
                     <div class="card-content">
-                        <div class="card-header">
-                            <div class="row">
-                                <div class="col-12 col-sm-7">
-                                    <h5 class="pt-2">View Quote Details</h5>
-                                </div>
-                                <div class="col-12 col-sm-5 d-flex justify-content-end align-items-center">
-                                    <a href="{{URL::previous()}}" class="btn btn-sm btn-primary px-3 py-1"><i class="fa fa-arrow-left"></i> Back</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <ul class="nav nav-tabs">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" data-toggle="tab" href="#data_details">Details</a>
-                                        </li>
-                                    </ul>
-                                    <div class="tab-content">
-                                        <div id="data_details" class="tab-pane fade in active show">
-                                            <div class="table-responsive">
-                                                <table class="table table-striped table-bordered">
-                                                    <tr>
-                                                        <td><strong>Quote Image</strong></td>
-                                                        <td><img src="{{$media->getFullUrl() ?? ''}}" width="200px" alt=""></td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td><strong>Quote Sequence</strong></td>
-                                                        <td>{{$quotes->sequence ?? ''}}</td>
-                                                    </tr>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
+                    	<div class="card-body">
+                            <div class="col-12">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-striped">
+                                        <tr>
+                                            <td><strong>Quote Image</strong></td>
+                                            <td><img src="{{$media->getFullUrl() ?? ''}}" width="200px" alt=""></td>
+                                        </tr>
+                                        <tr>
+                                            <td><strong>Quote Sequence</strong></td>
+                                            <td>{{$quotes->sequence ?? ''}}</td>
+                                        </tr>
+                                    </table>
                                 </div>
                             </div>
-                        </div>
+                    	</div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
